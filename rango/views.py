8525df805f2ec1a9c68cf206c5b2b3c537b82928
aidan <aidantcooper@gmail.com>
2017-04-22 09:56:06 +0100

    Updating to end of Chapter 6.

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,46 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
+from rango.forms import CategoryForm
+from rango.models import Category, Page
+
+
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    return render(request, 'rango/index.html', context=context_dict)
+    category_list = Category.objects.order_by('-likes')[:5]
+    most_viewed_pages = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list,
+                    'most_viewed_pages': most_viewed_pages}
+    return render(request, 'rango/index.html', context_dict)
+
 
 def about(request):
-	return render(request, 'rango/about.html')
+    return render(request, 'rango/about.html')
+
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
